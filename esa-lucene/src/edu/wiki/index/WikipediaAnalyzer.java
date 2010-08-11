2897cbcf778d4d372251a4a5b2a2b136381248a3
Çağatay Çallı <cagatay@nsyght.com>
2010-08-11 17:53:03 +0300

    changed indexer to use Dave Lewis' stop word list from file

--- esa-lucene/src/edu/wiki/index/WikipediaAnalyzer.java
+++ esa-lucene/src/edu/wiki/index/WikipediaAnalyzer.java
@@ -1,9 +1,11 @@
 package edu.wiki.index;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
-import java.util.Arrays;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Set;
 
 import org.apache.lucene.analysis.Analyzer;
@@ -22,33 +24,29 @@ public class WikipediaAnalyzer extends Analyzer {
 	
 	/** An unmodifiable set containing some common English words that are not usually useful
 	  for searching.*/
-	public static final Set<?> ENGLISH_STOP_WORDS_SET;
-
-	static {
-		final List<String> stopWords = Arrays.asList(	
-				"http","www","a","about","after","all","almost","along","already","also","although","always",
-				"am","among","an","and","any","another","anyone","anything","anywhere","are","around","as","at","away",
-				"back","be","because","between","beyond","both","but","by","can","cannot","come","could","d","did",
-				"do","does","doing","done","during","each","either","enough","etc","even","ever","every",
-				"everyone","everything","few","first","five","for","four","from","further","gave","get","getting",
-				"give","given","giving","go","goes","going","gone","good","got","great","had","has","have",
-				"having","here","high","how","however","i","if","ii","in","includes","including","indeed",
-				"instead","into","is","it","its","itself","just","less","least","like","likely","ll","long",
-				"m","made","make","many","may","maybe","me","might","mine","more","most","much","must","less",
-				"neither","never","new","no","nobody","noone","nor","not","nothing","now","of","old","on",
-				"once","one","only","or","other","others","our","ours","out","over","own","perhaps","put",
-				"rather","really","s","same","shall","said","say","says","second","see","seen","should","simply",
-				"since","so","some","someone","something","sometimes","somewhere","soon","still","such","take",
-				"than","that","the","their","theirs","then","there","therefore","these","they","thing","things",
-				"this","those","though","through","three","thus","to","together","too","toward","two","unless",
-				"until","upon","us","use","used","uses","using","usually","very","was","way","ways","we","well",
-				"went","were","what","when","whenever","where","wherever","whether","which","while","who",
-				"whoever","why","will","with","within","without","would","yes","yet","you","your","yours","yourself"
-		);
+	public final Set<?> ENGLISH_STOP_WORDS_SET;
+	
+	public WikipediaAnalyzer() throws IOException {
+		// read stop words
+		InputStream is = ESAWikipediaIndexer.class.getResourceAsStream("/config/stopwords.txt");
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		ArrayList<String> stopWords = new ArrayList<String>(500);
+		
+		String line;
+		
+		while((line = br.readLine()) != null){
+			stopWords.add(line);
+		}
+		
+		br.close();
+		
 		final CharArraySet stopSet = new CharArraySet(stopWords.size(), false);
 		stopSet.addAll(stopWords);  
-		ENGLISH_STOP_WORDS_SET = CharArraySet.unmodifiableSet(stopSet); 
+				
+		ENGLISH_STOP_WORDS_SET = CharArraySet.unmodifiableSet(stopSet);
+
 	}
+
 	
     public TokenStream reusableTokenStream(
         String fieldName, Reader reader) throws IOException {
