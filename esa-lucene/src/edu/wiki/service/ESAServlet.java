abdd1e84aac8c316e2c836cc7a1e66d236b06bd5
Çağatay Çallı <cagatay@nsyght.com>
2010-08-31 11:14:18 +0300

    added a task to retrieve concept vectors to ESA web service

--- esa-lucene/src/edu/wiki/service/ESAServlet.java
+++ esa-lucene/src/edu/wiki/service/ESAServlet.java
@@ -1,10 +1,18 @@
 package edu.wiki.service;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.HashMap;
 import java.util.Locale;
 
 import javax.servlet.ServletConfig;
@@ -29,6 +37,8 @@ import org.w3c.dom.Element;
 import com.sun.org.apache.xerces.internal.dom.DocumentImpl;
 import com.sun.org.apache.xerces.internal.parsers.DOMParser;
 
+import edu.wiki.api.concept.IConceptIterator;
+import edu.wiki.api.concept.IConceptVector;
 import edu.wiki.index.WikipediaAnalyzer;
 import edu.wiki.search.ESASearcher;
 import edu.wiki.search.NormalizedWikipediaDistance;
@@ -47,6 +57,36 @@ public class ESAServlet extends HttpServlet {
 	protected DecimalFormat df = (DecimalFormat) NumberFormat.getInstance(Locale.US);
 	
 	
+	
+	static Connection connection;
+	static Statement stmtQuery;
+	
+	static String strTitles = "SELECT id,title FROM article WHERE id IN ";
+	
+	public static void initDB() throws ClassNotFoundException, SQLException, IOException {
+		// Load the JDBC driver 
+		String driverName = "com.mysql.jdbc.Driver"; // MySQL Connector 
+		Class.forName(driverName); 
+		
+		// read DB config
+		InputStream is = ESASearcher.class.getResourceAsStream("/config/db.conf");
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		String serverName = br.readLine();
+		String mydatabase = br.readLine();
+		String username = br.readLine(); 
+		String password = br.readLine();
+		br.close();
+
+		// Create a connection to the database 
+		String url = "jdbc:mysql://" + serverName + "/" + mydatabase; // a JDBC url 
+		connection = DriverManager.getConnection(url, username, password);
+		
+		stmtQuery = connection.createStatement();
+		stmtQuery.setFetchSize(100);
+
+  }
+	
+	
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
 		context = config.getServletContext() ;
@@ -54,6 +94,7 @@ public class ESAServlet extends HttpServlet {
 		nwd = new NormalizedWikipediaDistance(context.getInitParameter("index_path"));
 		try {
 			esa = new ESASearcher();
+			initDB();
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new ServletException();
@@ -115,6 +156,62 @@ public class ESAServlet extends HttpServlet {
 					final double sim = esa.getRelatedness(term1, term2);
 					response.setContentType("text/html");
 					response.getWriter().append(df.format(sim)) ;
+					return ;
+				}
+
+			}
+			
+			//process compare request
+			if (task.equals("vector")) {
+				String source = request.getParameter("source");
+				final int limit = Integer.valueOf(request.getParameter("limit"));
+				
+				if (source == null) {
+					response.setContentType("text/html");
+					response.getWriter().append("null") ;
+					return ;
+				}
+				else {					
+					response.setContentType("text/html");
+					
+					final IConceptVector cv = esa.getConceptVector(source);
+					
+					if(cv == null){
+						response.getWriter().append("null") ;
+					}
+					
+					else {
+						final IConceptVector ncv = esa.getNormalVector(cv, 20);
+						final IConceptIterator it = ncv.orderedIterator();
+						
+						HashMap<Integer, Double> vals = new HashMap<Integer, Double>(10);
+						HashMap<Integer, String> titles = new HashMap<Integer, String>(10);
+						
+						String inPart = "(";
+						
+						int count = 0;
+						while(it.next() && count < limit){
+							inPart += it.getId() + ",";
+							vals.put(it.getId(),it.getValue());
+							count++;
+						}
+						
+						inPart = inPart.substring(0,inPart.length()-1) + ")";
+								
+						ResultSet r = stmtQuery.executeQuery(strTitles + inPart);
+						while(r.next()){
+							titles.put(r.getInt(1), new String(r.getBytes(2),"UTF-8")); 
+						}
+						
+						
+						it.reset();
+						count = 0;
+						while(it.next() && count < limit){
+							int id = it.getId();
+							response.getWriter().append(id + "\t" + titles.get(id) + "\t" + df.format(vals.get(id)) + "\n") ;
+							count++;
+						}
+					}
 					return ;
 				}
 
